868c5640b62611b7f0a5af4b57363950e19dd6ee
nlaz <nikolazaris@utexas.edu>
2016-04-17 12:18:21 -0500

    add env var

--- facebook_bot.js
+++ facebook_bot.js
@@ -164,14 +164,8 @@ var QUESTIONS = {
     ]
 }
 
-if (!process.env.page_token) {
-    console.log('Error: Specify page_token in environment');
-    process.exit(1);
-}
-
-if (!process.env.verify_token) {
-    console.log('Error: Specify verify_token in environment');
-    process.exit(1);
+if(!process.env.page_token || !process.env.verify_token) {
+  var env = require('./env.js')
 }
 
 
